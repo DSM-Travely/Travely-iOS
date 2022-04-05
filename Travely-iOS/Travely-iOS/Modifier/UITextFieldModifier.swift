c6a61fc716a1ed44e71c0f9f355074e4740215fc
jangseoyoung <jangseoyoung0402@gmail.com>
2022-04-05 23:41:21 +0900

    [CREATE] UITextFieldModifier

---
+++ Travely-iOS/Travely-iOS/Modifier/UITextFieldModifier.swift
@@ -0,0 +1,17 @@
+//
+//  UITextField.swift
+//  Travely-iOS
+//
+//  Created by 장서영 on 2022/04/05.
+//
+
+import SwiftUI
+
+struct StandardTextField: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 300, height: 40, alignment: .leading)
+            .border(Color.gray)
+            .cornerRadius(10)
+    }
+}
