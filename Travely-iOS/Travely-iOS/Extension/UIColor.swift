ef60989a769a68087b76915577007044117f3e35
jangseoyoung <jangseoyoung0402@gmail.com>
2022-04-05 23:41:30 +0900

    [CREATE] UIColor Extension

---
+++ Travely-iOS/Travely-iOS/Extension/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor.swift
+//  Travely-iOS
+//
+//  Created by 장서영 on 2022/04/05.
+//
+
+import UIKit
+
+extension UIColor {
+    class var mainColor: UIColor? { return UIColor(named: "MainColor") }
+    class var skyBlueColor: UIColor? { return UIColor(named: "SkyBlueColor") }
+}
